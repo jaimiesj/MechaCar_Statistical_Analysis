50202f2294f0d5bda4200b53b6e02259491f4568
jaimiesj <88955412+jaimiesj@users.noreply.github.com>
2021-12-06 00:20:48 -0500

    RScript

---
+++ MechaCarChallenge.R
@@ -0,0 +1,14 @@
+library(dplyr)
+mecha_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+lm(mpg ~ AWD + ground_clearance + spoiler_angle + vehicle_weight + vehicle_length,data=mecha_mpg) #generate multiple linear regression model
+summary(lm(mpg ~ AWD + ground_clearance + spoiler_angle + vehicle_weight + vehicle_length,data=mecha_mpg)) #generate summary statistics
+
+suspension <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+library(tidyverse)
+total_summary <- suspension %>% summarize(Mean_Psi=mean(PSI), Median_Psi=median(PSI), var_PSI=var(PSI), sd_PSI=sd(PSI), .groups = 'keep')
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean_Psi=mean(PSI), Median_Psi=median(PSI), var_PSI=var(PSI), sd_PSI=sd(PSI), .groups = 'keep')
+t.test(suspension$PSI, mu=1500)
+Lot_1 <- subset(suspension$PSI, suspension$Manufacturing_Lot == "Lot 1")
+t.test(suspension$PSI, mu=1500, subset= suspension$Manufacturing_Lot == "Lot 1")
+t.test(suspension$PSI, mu=1500, subset= suspension$Manufacturing_Lot == "Lot 2")
+t.test(suspension$PSI, mu=1500, subset= suspension$Manufacturing_Lot == "Lot 3")
